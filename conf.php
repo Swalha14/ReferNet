8155fe8fd9841d6bd6795394d1504f8f0bd5ebcf
Swalha <swalhambarak887@gmail.com>
2026-07-03 00:21:55 +0300

    Removing sensitive details

--- conf.php
+++ conf.php
@@ -13,13 +13,13 @@ $conf['smtp_user'] = '';
 $conf['smtp_pass'] = ''; // Gmail App Password
 $conf['smtp_port'] = 465;
 $conf['smtp_secure'] = 'ssl';
-//mysql://root:agdiXaBUGIHqerfXXkjUVgAsbgjTYOrF@metro.proxy.rlwy.net:10661/railway
+
 
 $conf['db_type'] = 'PDO';
-$conf['db_host'] = 'metro.proxy.rlwy.net';
-$conf['db_port'] = 10661;
-$conf['db_user'] = 'root';
-$conf['db_pass'] = 'agdiXaBUGIHqerfXXkjUVgAsbgjTYOrF';
+$conf['db_host'] = '';
+$conf['db_port'] = '';
+$conf['db_user'] = '';
+$conf['db_pass'] = '';
 $conf['db_name'] = 'refernet';
 
 //database connection
